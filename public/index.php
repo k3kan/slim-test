5cbb8384606130cca2909cc66f3cf3c8edd58509
K3kan <aintbor@gmail.com>
2020-11-09 20:12:38 +0300

    add search users

--- public/index.php
+++ public/index.php
@@ -4,9 +4,18 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория, в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
+
 
 $app->get('/', function ($request, $response) {
     $response->getBody()->write('Welcome to Slim!');
@@ -14,4 +23,35 @@ $app->get('/', function ($request, $response) {
     // Благодаря пакету slim/http этот же код можно записать короче
     // return $response->write('Welcome to Slim!');
 });
+
+$app->get('/courses/{id}', function ($request, $response, array $args) {
+    $id = $args['id'];
+    return $response->write("Course id: {$id}");
+});
+
+$app->get('/users', function ($request, $response) use($users) {
+    $term = $request->getQueryParam('term');
+    if (empty($term)) {
+        $courses = $users;
+    }
+    else {
+        $courses = array_filter($users, function ($name) use($term) {
+            $pos = strpos(strtolower($name), strtolower($term));
+            if ($pos !== false) {
+                return $name;
+            }
+        });
+    }
+    $params = ['courses' => $courses, 'term' => $term];
+    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
+});
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
+    // $this доступен внутри анонимной функции благодаря https://php.net/manual/ru/closure.bindto.php
+    // $this в Slim это контейнер зависимостей
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+});
+
 $app->run();
